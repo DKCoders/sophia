1bfa41e9835490fc0d1154d5bf3b4895d5184882
Alexys Gonzalez <alexysbike@gmail.com>
2018-03-02 00:40:13 -0300

    User service done

--- user/services/user.js
+++ user/services/user.js
@@ -11,6 +11,36 @@ class UserService {
     }
     return mapResponse('docs', await User.paginate(find, options));
   }
+
+  static async getById(userId) {
+    const document = await User.findById(userId);
+    return mapResponse('document', {document});
+  }
+
+  static async create(user) {
+    const document = await User.create(user);
+    return mapResponse('document', {document});
+  }
+
+  static async updateById(userId, user, overwrite = false) {
+    const document = await User.findByIdAndUpdate(userId, user, {overwrite, new: true});
+    const doc = !document 
+      ? null 
+      : await User.findByIdAndUpdate(userId, {'audit._updatedAt': new Date()}, {new: true});
+    return mapResponse('doc', {doc});
+  }
+
+  static async update(query, body, options) {
+    body.$set = body.$set || {};
+    body.$setOnInsert = body.$setOnInsert || {};
+    Object.assign(body.$set, {
+        'audit._updatedAt': new Date()
+    });
+    return {
+        data: {},
+        meta: await User.update(query, body, options)
+    };
+}
 }
 
 module.exports = UserService;
