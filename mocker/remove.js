9388992c01f40ff52b5193f46a89a735bab9f5b5
Alexys Gonzalez <alexysbike@gmail.com>
2018-03-06 23:28:13 -0300

    Adding orders to mocker

--- mocker/remove.js
+++ mocker/remove.js
@@ -6,20 +6,23 @@ const Brand = mongoose.model('Brand');
 const Category = mongoose.model('Category');
 const Product = mongoose.model('Product');
 const Client = mongoose.model('Client');
+const Order = mongoose.model('Order');
 
 const remove = async () => {
   const mongoDBUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/sophia';
   await mongoose.connect(mongoDBUri);
-  console.log('Removing users');
+  console.log('Removing Users');
   await User.remove({});
-  console.log('Removing brands');
+  console.log('Removing Brands');
   await Brand.remove({});
-  console.log('Removing categories');
+  console.log('Removing Categories');
   await Category.remove({});
-  console.log('Removing products');
+  console.log('Removing Products');
   await Product.remove({});
-  console.log('Removing clients');
+  console.log('Removing Clients');
   await Client.remove({});
+  console.log('Removing Orders');
+  await Order.remove({});
   process.exit(0);
 };
 
